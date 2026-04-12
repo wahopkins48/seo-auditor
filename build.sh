f6ce39e9b973b1934c4d22a954c4f7606d1f01dc
Wesley Hopkins <wesley@Mac.lan>
2026-04-12 12:50:28 -0500

    Force sync all local changes to cloud

--- build.sh
+++ build.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
-# exit on error
 set -o errexit
 
-# 1. Install Python packages
 pip install -r requirements.txt
-
-# 2. Install Playwright and its specific dependencies
-# (Playwright's internal command is allowed on Render)
 playwright install chromium
-playwright install-deps
